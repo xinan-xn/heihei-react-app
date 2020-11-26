d3abb9c8a4e8402730a2e83b9f8224275a7f52b4
xinan <781755941@qq.com>
2020-11-26 08:59:01 +0800

    初始化

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './css/one.css'
+import Header from './view/xuenan/index'
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
