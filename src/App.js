609141f35bcbc3f2d1b4b314cfaf9da7d04ee275
cong <2537504870@qq.com>
2020-11-27 10:24:51 +0800

    段毅聪周五

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import './css/one.css'
 import Header from './view/xuenan/index'
 // import Detail from './view/yango/detail'
 // import Head from './view/xuenan/head'
 import Nav from './view/xuenan/nav'
-import Banner from './component/Banner'
+// import Banner from './component/Banner'
 // import { Route } from 'react-router-dom'
 import axio from './http'
+
+import Home from "../src/view/dyc/home"
 axio().then((res) => {
   console.log(res)
 })
@@ -15,32 +17,33 @@ axio().then((res) => {
 //   transform: 'translateX(4.5rem)'
 // }
 function App() {
-  const [flag,setFlag]=useState(false)
-  useEffect(()=>{
+  const [flag, setFlag] = useState(false)
+  useEffect(() => {
     console.log(flag);
-  },[flag])
+  }, [flag])
   return (
     <div className="App">
       {/* <Detail></Detail> */}
-      <Header></Header>
+      {/* <Header></Header> */}
       <Header setFlag={setFlag} flag={flag}></Header>
       {/* <Head></Head> */}
       {
-        flag?<Nav></Nav>:""
+        flag ? <Nav></Nav> : ""
       }
-      
+
       <div style={
-       {
-        transform: `translateX(${flag?4.5:0}rem)`
-       }
+        {
+          transform: `translateX(${flag ? 4.5 : 0}rem)`
+        }
       }>
-      <Banner ></Banner>
+        {/* <Banner ></Banner> */}
+        <Home></Home>
       </div>
       {/* <Route path="/" exact component={首页}></Route>
       <Route path="/course" exact component={课程表}></Route>
       <Route path="/lecturer" exact component={讲师}></Route> */}
     </div>
+
   );
 }
-
-export default App;
+export default App
